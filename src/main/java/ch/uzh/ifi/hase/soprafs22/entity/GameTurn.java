5aece677af5818da4fe1478e6287a5910104424e
shmnrr <91474287+shmnrr@users.noreply.github.com>
2022-04-25 22:08:56 +0200

    Update GameTurn.java

--- src/main/java/ch/uzh/ifi/hase/soprafs22/entity/GameTurn.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/entity/GameTurn.java
@@ -2,29 +2,30 @@ package ch.uzh.ifi.hase.soprafs22.entity;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import ch.uzh.ifi.hase.soprafs22.constant.BallState;
 import ch.uzh.ifi.hase.soprafs22.constant.Rank;
 
 public class GameTurn {
 
-    // TODO: I guess we go with PlayerState instead of player everywhere?
+    // TODO: MAYBE NEEDS TO BE SEPARATED INTO MORE CLASSES - TOO MUCH CLUTTER
 
-    // TODO: I HAVE IMPLEMENTED HIGHLIGHTING AS FOR THE WHOLE PLAYERHAND
-    // I JUST REALISED IT SHOULD RATHER BE FOR A SPECIFIC PICKED CARD
-    // TO AVOID CLUTTERED BOARD HIGHLIGHT I GUESS?
-
-    // TODO: NEEDS TO BE SEPARATED INTO MORE CLASSES - TOO MUCH CLUTTER
+    // When making a move player:
+    // 1. Chooses card => possible moves are highlighted
+    // 2. Can switch to another card
+    // 3. Chooses ball with which to move
+    // TODO: 4. Clicks on one of the highlighted positions to go there ???
 
     private PlayerState activePlayer;
     private PlayerHand playerHand;
     private Boolean canInvoke;
 
-    private List<Integer> possibleMoves = new ArrayList<Integer>();
-
     // Normal cards: TWO, THREE, FIVE, SIX, EIGHT, NINE, TEN, QUEEN
+    // TODO: THIS COULD BE SEPARATE
     HashMap<String, Integer> normalCards = (HashMap<String, Integer>) Map.of("TWO", 2,
             "THREE", 3,
             "FIVE", 5,
@@ -45,42 +46,80 @@ public class GameTurn {
         canInvoke = true;
     }
 
-    public void playCard(Card card, PlayerState activePlayer) {
+    public void playCard(Card card, PlayerState activePlayer, Ball ball) {
         // TODO: will need to remove card from player's hand and
         // make move with makeMove
-        // have to remove exact card; need to take care of possible multiplicities in
-        // hand
-        makeMove(card.getRank());
+        // have to remove exact card; need to take care of possible multiplicities in hand
+        makeMove(card, ball);
         playerHand.deleteCard(card);
     }
 
-    // FOR THE WHOLE HAND
-    public List<Integer> calculateMoves() {
+    public void makeMove(Card card, Ball ball) {
+        // TODO: move with a given card
 
-        // get player's cards
-        List<Card> activeCards = playerHand.getActiveCards();
+        int startPos = ball.getPosition();
 
-        // for each card in the hand calculate the possible moves
-        for (Card card : activeCards) {
+        calculateMove(card);
 
-            Rank cardRank = card.getRank();
+        // TODO: length of the move
+        int moveLength = 0;
+        
+        //int postPos = ball.setPosition(startPos + move);
 
-            getPossibleMoves(cardRank);
-        }
+        // Check if any ball on the way on starting point
+        // TODO: DETERMINE WHICH POSITIONS WILL BE STARTING POINTS
+        // ATM RANDOM VALUES
+        // ArrayList<Integer> startingPoints = (ArrayList<Integer>) List.of(0,10,20,30);
 
-        return possibleMoves;
+        // int startPos = ball.getPosition();
+
+        // for (int possibleMove : possibleMoves) {
+        //     for (int i = startPos + 1; i <= startPos + moveLength; i++) {
+        //         for (Ball ball : balls) {
+        //             if (startingPoints.contains(ball.getPosition())) {
+        //                 possibleMoves.remove(possibleMove);
+        //             }
+        //         }
+        //     }
+        // }
+
+        // CHECK IF KILLED ANYTHING WHEN MOVING
+        // for (Ball ball : balls) {}
+        // if (ball.getPosition() == )
     }
 
     // FOR SINGLE CARD
     public List<Integer> calculateMove(Card card) {
 
         Rank cardRank = card.getRank();
-        getPossibleMoves(cardRank);
-
-        return possibleMoves;
+        if (cardRank != Rank.JOKER) {
+            return getPossibleMoves(cardRank);
+        }
+        else {
+            Rank jokerRank = chooseJokerRank();
+            return getPossibleMoves(jokerRank);
+        }
     }
 
-    public void getPossibleMoves(Rank cardRank) {
+    // FOR THE WHOLE HAND
+    // public List<Integer> calculateMoves() {
+
+    //     // get player's cards
+    //     List<Card> activeCards = playerHand.getActiveCards();
+
+    //     // for each card in the hand calculate the possible moves
+    //     for (Card card : activeCards) {
+
+    //         Rank cardRank = card.getRank();
+    //         getPossibleMoves(cardRank);
+    //     }
+
+    //     return possibleMoves;
+    // }
+
+    public List<Integer> getPossibleMoves(Rank cardRank) {
+        List<Integer> possibleMoves = new ArrayList<Integer>();
+
         if (normalCards.get(cardRank) != null) {
             possibleMoves.add(normalCards.get(cardRank));
         }
@@ -89,6 +128,7 @@ public class GameTurn {
             possibleMoves.add(-4);
         }
         // TODO: implement cooperation with team member
+        // and killing on the way
         else if (cardRank.equals(Rank.SEVEN)) {
             possibleMoves.add(1);
             possibleMoves.add(2);
@@ -104,7 +144,7 @@ public class GameTurn {
             // target area, may not be exchanged.
 
             // TODO: COMPLETE IMPLEMENTATION OF THIS WILL REQUIRE BOARDSTATE
-            // for (ball : balls) {
+            // for (Ball ball : balls) {
             // if (ball.getState() == BallState.BOARD) {
             // possibleMoves.add(ball.getPosition());
             // }
@@ -120,14 +160,25 @@ public class GameTurn {
             possibleMoves.add(11);
             setInvokeTrue();
         } 
-        else if (cardRank.equals(Rank.JOKER)) {
-            // TODO: CAN BE ANY OF ABOVE CARDS
+        // else if (cardRank.equals(Rank.JOKER)) {
+        //     // TODO: CAN BE ANY OF ABOVE CARDS
+        // }
 
-        }
+        // Remove duplicates from possible moves
+        Set<Integer> set = new LinkedHashSet<>();
+        set.addAll(possibleMoves);
+        possibleMoves.clear();
+        possibleMoves.addAll(set);
+
+        return possibleMoves;
     }
 
-    public void makeMove(Rank cardRank) {
-        // TODO: move with a given card
+    public Rank chooseJokerRank() {
+        // TODO: WILL NEED TO SHOW THE USER POSSIBLE CARDS
+        // OR JUST HIGHLIGHT THE MOVES ???
+        Rank chosenRank = Rank.TWO;
+        System.out.println("User chooses joker's rank");
+        return chosenRank;
     }
 
     public void endTurn() {
