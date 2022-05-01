aa625621ca838e55a89e386101dc6818159ca80a
shmnrr <91474287+shmnrr@users.noreply.github.com>
2022-05-01 22:12:31 +0200

    Card, Deck, GameTurn, PlayerHand, PlayerState

--- src/test/java/ch/uzh/ifi/hase/soprafs22/entities/EntitiesTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs22/entities/EntitiesTest.java
@@ -1,9 +1,11 @@
 package ch.uzh.ifi.hase.soprafs22.entities;
 
 import ch.uzh.ifi.hase.soprafs22.constant.Color;
-import ch.uzh.ifi.hase.soprafs22.entity.Ball;
-import ch.uzh.ifi.hase.soprafs22.entity.BoardState;
+import ch.uzh.ifi.hase.soprafs22.constant.Rank;
+import ch.uzh.ifi.hase.soprafs22.constant.Suit;
+import ch.uzh.ifi.hase.soprafs22.entity.*;
 import org.junit.jupiter.api.Test;
+import org.springframework.security.core.parameters.P;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -37,6 +39,93 @@ public class EntitiesTest {
 
         assertEquals(balls, boardState.getBalls());
         //assertEquals(null, boardState.getBallById(null));
+
+    }
+
+    @Test
+    public void CardTest() {
+
+        Card card = new Card(Rank.ACE, Suit.CLUB);
+
+        assertEquals(Rank.ACE, card.getRank());
+        assertEquals(Suit.CLUB, card.getSuit());
+
+    }
+
+    @Test
+    public void DeckTest() {
+
+        Card card = new Card(Rank.ACE, Suit.CLUB);
+
+        Deck deck = new Deck();
+
+        for (int i = 0; i <= 52; i++) {
+            if (deck.drawCard().equals(card)) {
+                assert(deck.drawCard() instanceof Card);
+            }
+        }
+
+    }
+
+    @Test
+    public void GameTurnTest() {
+
+        PlayerState activePlayer = new PlayerState();
+
+        BoardState boardState = new BoardState();
+
+        GameTurn gameTurn = new GameTurn(activePlayer, boardState);
+    }
+
+    @Test
+    public void PlayerHandTest() {
+
+        PlayerHand playerHand = new PlayerHand();
+
+        Card card = new Card(Rank.ACE, Suit.CLUB);
+
+        HashSet<Card> cards = new HashSet<>(Set.of(card));
+        HashSet<Card> cardsEmpty = new HashSet<>(Set.of());
+
+        playerHand.drawCards(cards);
+        assertEquals(cards, playerHand.getActiveCards());
+
+        playerHand.deleteCard(card);
+        assertEquals(cardsEmpty, playerHand.getActiveCards());
+
+    }
+
+    @Test
+    public void PlayerStateTest() {
+
+        User player = new User();
+        PlayerHand playerHand = new PlayerHand();
+
+        Card card = new Card(Rank.ACE, Suit.CLUB);
+
+        HashSet<Card> cards = new HashSet<>(Set.of(card));
+
+        PlayerState playerState = new PlayerState(player, 1, Color.GREEN, true, playerHand);
+
+        assertEquals(false, playerState.getIsPlaying());
+        playerState.setIsPlaying(true);
+        assertEquals(true, playerState.getIsPlaying());
+
+        assertEquals(1, playerState.getTeam());
+        playerState.setTeam(2);
+        assertEquals(2, playerState.getTeam());
+
+        assertEquals(true, playerState.getPlayerStatus());
+        playerState.setPlayerStatus(false);
+        assertEquals(false, playerState.getPlayerStatus());
+
+        assertEquals(Color.GREEN, playerState.getColor());
+        playerState.setColor(Color.RED);
+        assertEquals(Color.RED, playerState.getColor());
+
+        assertEquals(playerHand, playerState.getPlayerHand());
+        playerState.drawCards(cards);
+        assertEquals(playerHand, playerState.getPlayerHand());
 
     }
 
